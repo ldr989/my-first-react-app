849fdc000c099e40b9ad89237c79dcaeedc78dde
LDR <89.freedom@gmail.com>
2024-02-06 12:56:32 +0600

    add the ability to dynamically change classes

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,14 +1,14 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
-const EmployeesList = () => {
-    return (
-        <ul className="app-list list-group">
-            <EmployeesListItem />
-            <EmployeesListItem />
-            <EmployeesListItem />
-        </ul>
-    );
+const EmployeesList = ({ data }) => {
+    const elements = data.map((item) => {
+        // return <EmployeesListItem name={item.name} salary={item.salary} />;
+        const { id, ...itemProps } = item;
+        return <EmployeesListItem key={id} {...itemProps} />;
+    });
+
+    return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployeesList;
